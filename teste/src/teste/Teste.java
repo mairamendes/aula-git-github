b47265a033d415b86a8c4599458405db10ed67c7
Maíra Mendes <109531496+mairamendes@users.noreply.github.com>
2024-09-02 20:20:57 -0300

    Update Teste.java
    Soma de números

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -3,8 +3,8 @@ package teste;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num1 = 3 num2 = 5, soma;
+		soma = num1 + num2;
 	}
 
 }
